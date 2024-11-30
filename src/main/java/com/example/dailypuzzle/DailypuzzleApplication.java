fa920bba929fc8cb8730d0d83e50aa3f968eeccb
yasmine832 <yasmine.el.mesaoudi.uasani@student.ehb.be>
2024-11-30 15:56:28 +0100

    adds database info

--- src/main/java/com/example/dailypuzzle/DailypuzzleApplication.java
+++ src/main/java/com/example/dailypuzzle/DailypuzzleApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DailypuzzleApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DailypuzzleApplication.class, args);
+
+        SpringApplication.run(DailypuzzleApplication.class, args); // start app
     }
 
 }
