441df3d096063ed1c3d17f463bae26f26047c4ea
yasmine832 <yasmine.el.mesaoudi.uasani@student.ehb.be>
2024-11-30 15:16:56 +0100

    start

---
+++ src/main/java/com/example/dailypuzzle/DailypuzzleApplication.java
@@ -0,0 +1,13 @@
+package com.example.dailypuzzle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailypuzzleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailypuzzleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dailypuzzle/DailypuzzleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dailypuzzle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailypuzzleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
