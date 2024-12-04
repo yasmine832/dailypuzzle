381433903d160b039dfb10050447805ba7125166
yasmine832 <yasmine.el.mesaoudi.uasani@student.ehb.be>
2024-12-04 23:24:17 +0100

    validation user

--- src/main/java/com/example/dailypuzzle/model/User.java
+++ src/main/java/com/example/dailypuzzle/model/User.java
@@ -2,6 +2,9 @@ package com.example.dailypuzzle.model;
 
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -18,15 +21,22 @@ public class User implements UserDetails { //interface required by spring securi
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) // db is responsible for generating id
     private Long id;
+
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters")
     @Column(unique = true, nullable = false)
     private String username;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
     @Column(unique = true, nullable = false)
     private String email;
+
     @Column(nullable = false)
     private String password;
 
     @Column(nullable = false)
-    private String role; //eg "ROLE_USER" "ROLE_ADMIN"
+    private String role = "ROLE_USER"; //by default
 
 
     @Column(name="chosen time", nullable = false)
