9ae4179430873da893cf05630512111ca4c9c6af
yasmine832 <yasmine.el.mesaoudi.uasani@student.ehb.be>
2024-12-08 15:59:34 +0100

    templates

--- src/main/java/com/example/dailypuzzle/controller/UserController.java
+++ src/main/java/com/example/dailypuzzle/controller/UserController.java
@@ -10,10 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.context.request.WebRequest;
-import org.springframework.ui.ModelMap;
 
 @Controller
 public class UserController {
